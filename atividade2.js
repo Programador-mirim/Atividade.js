a510c2ed1a75dcb7976254acdfaaf1b0796a1db5
Sammy Calebe Diniz Quadros <sammy.quadros@aluno.senai.br>
2025-06-26 16:13:16 -0300

    segundo código

---
+++ atividade2.js
@@ -0,0 +1,14 @@
+let num1 = 1
+let num2 = 2
+let soma = num1 + num2
+let subtracao = num1 - num2
+let multiplicacao = num1 * num2
+let divisao = num1 / num2
+let potenciacao = num1 ** num2
+
+// Mostre todas as operações
+console.log(num1 + " + " + num2 + " = " + soma)
+console.log(num1 + " - " + num2 + " = " + subtracao)
+console.log(num1 + " * " + num2 + " = " + multiplicacao)
+console.log(num1 + " / " + num2 + " = " + divisao.toFixed(2))
+console.log(num1 + " ** " + num2 + " = " + potenciacao)
