21527535b80a6ebc3b4a05b283e19f40dbc5c062
Sammy Calebe Diniz Quadros <sammy.quadros@aluno.senai.br>
2025-06-26 15:06:26 -0300

    primeiro Código

--- atividade1.js
+++ atividade1.js
@@ -1,1 +1,10 @@
-//ainda vou fazer
+let nome = "Sammy"
+let idade = 18
+let esta_estudando = true
+
+if (esta_estudando == true) {
+    console.log("Olá, " + nome + ", você tem " + idade + " anos e está estudando")
+}
+else {
+    console.log("Olá, " + nome + ", você tem " + idade + " anos e não está estudando")
+}
