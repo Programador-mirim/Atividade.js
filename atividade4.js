aa972d1c04167b949ce73c03b3482eb54d6c9a7f
Sammy Calebe Diniz Quadros <sammy.quadros@aluno.senai.br>
2025-08-07 17:16:28 -0300

    envio de atividades

---
+++ atividade4.js
@@ -0,0 +1,10 @@
+let nota = 7
+if (nota >= 7) {
+    console.log("Aprovado")
+}
+else if (nota >= 5 && nota <= 6.9) {
+    console.log("Recuperação")
+}
+else {
+    console.log("Reprovado")
+}
---
+++ atividade5.js
@@ -0,0 +1,5 @@
+let num1 = 1
+let num2 = 2
+let soma = num1 + num2
+
+console.log(num1 + " + " + num2 + " = " + soma)
